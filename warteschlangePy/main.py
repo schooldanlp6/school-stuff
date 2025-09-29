fbf999047d6c9685d89a622a8d2dd1186dc411fb
DanLP6 <school.danlp6@outlook.de>
2025-09-29 11:15:26 +0200

    latest

---
+++ warteschlangePy/main.py
@@ -0,0 +1,18 @@
+import ui
+
+class Warteschlange():
+    def __init__(self):
+        pass
+
+class Knoten():
+    def __init__(self):
+        pass
+
+class Lied():
+
+    def __init__(self):
+        pass
+
+
+
+ui.boot()
